727e256da38250b89cd6ad75858be87530d8da8b
IanMcT <ian.mctavish@tldsb.on.ca>
2018-02-09 09:51:09 -0500

    Create sample.cs

---
+++ sample.cs
@@ -0,0 +1,14 @@
+int x = 2;
+if (x > 0)
+{
+  MessageBox.Show("greater!");
+}
+
+if (x <= 0) { MessageBox.Show("less than greater!"); }
+
+
+if (x == 0) { MessageBox.Show("Equal!"); }
+else if (x == 1) { MessageBox.Show("it is 1!"); } 
+else {
+  MessageBox.Show("nothing fit!");
+}
